ce19746ccdc86217dd40191fb687c7002fb89b0d
Sayan Deb Kundu <42212360+sayan-IIT@users.noreply.github.com>
2023-11-05 18:14:49 +0530

    Update Schedules.py

--- benchmark/application/coffee.py
+++ benchmark/application/coffee.py
@@ -3,91 +3,91 @@ from gurobipy import GRB, Model
 
 # Example data
 
-capacity_in_supplier = {'supplier1': 150, 'supplier2': 50, 'supplier3': 100}
+capacity_in_technician = {'Adam': 150, 'John': 50, 'Dave': 100}
 
-shipping_cost_from_supplier_to_roastery = {
-    ('supplier1', 'roastery1'): 5,
-    ('supplier1', 'roastery2'): 4,
-    ('supplier2', 'roastery1'): 6,
-    ('supplier2', 'roastery2'): 3,
-    ('supplier3', 'roastery1'): 2,
-    ('supplier3', 'roastery2'): 7
+Technician_Cost = {
+    ('Adam', 'Internal'): 5,
+    ('Adam', 'External'): 4,
+    ('John', 'Internal'): 6,
+    ('John', 'External'): 3,
+    ('Dave', 'Internal'): 2,
+    ('Dave', 'External'): 7
 }
 
-roasting_cost_light = {'roastery1': 3, 'roastery2': 5}
+maintenance_time= {'Internal': 3, 'External': 5}
 
-roasting_cost_dark = {'roastery1': 5, 'roastery2': 6}
+repair_time= {'Internal': 5, 'External': 6}
 
-shipping_cost_from_roastery_to_cafe = {
-    ('roastery1', 'cafe1'): 5,
-    ('roastery1', 'cafe2'): 3,
-    ('roastery1', 'cafe3'): 6,
-    ('roastery2', 'cafe1'): 4,
-    ('roastery2', 'cafe2'): 5,
-    ('roastery2', 'cafe3'): 2
+Travel_Cost = {
+    ('Internal', 'Customer_Site_1'): 5,
+    ('Internal', 'Customer_Site_2'): 3,
+    ('Internal', 'Customer_Site_3'): 6,
+    ('External', 'Customer_Site_1'): 4,
+    ('External', 'Customer_Site_2'): 5,
+    ('External', 'Customer_Site_3'): 2
 }
 
-light_coffee_needed_for_cafe = {'cafe1': 20, 'cafe2': 30, 'cafe3': 40}
+maintenance_work = {'Customer_Site_1': 20, 'Customer_Site_2': 30, 'Customer_Site_3': 40}
 
-dark_coffee_needed_for_cafe = {'cafe1': 20, 'cafe2': 20, 'cafe3': 100}
+repair_work = {'Customer_Site_1': 20, 'Customer_Site_2': 20, 'Customer_Site_3': 100}
 
-cafes = list(set(i[1] for i in shipping_cost_from_roastery_to_cafe.keys()))
-roasteries = list(
-    set(i[1] for i in shipping_cost_from_supplier_to_roastery.keys()))
-suppliers = list(
-    set(i[0] for i in shipping_cost_from_supplier_to_roastery.keys()))
+customers = list(set(i[1] for i in Travel_Cost.keys()))
+skills = list(
+    set(i[1] for i in Technician_Cost.keys()))
+technicians = list(
+    set(i[0] for i in Technician_Cost.keys()))
 
 # Create a new model
-model = Model("coffee_distribution")
+model = Model("GBS Field Scheduling")
 
 # OPTIGUIDE DATA CODE GOES HERE
 
 # Create variables
-x = model.addVars(shipping_cost_from_supplier_to_roastery.keys(),
+x = model.addVars(Technician_Cost.keys(),
                   vtype=GRB.INTEGER,
                   name="x")
-y_light = model.addVars(shipping_cost_from_roastery_to_cafe.keys(),
+y_light = model.addVars(Travel_Cost.keys(),
                         vtype=GRB.INTEGER,
                         name="y_light")
-y_dark = model.addVars(shipping_cost_from_roastery_to_cafe.keys(),
+y_dark = model.addVars(Travel_Cost.keys(),
                        vtype=GRB.INTEGER,
                        name="y_dark")
 
 # Set objective
 model.setObjective(
-    sum(x[i] * shipping_cost_from_supplier_to_roastery[i]
-        for i in shipping_cost_from_supplier_to_roastery.keys()) +
-    sum(roasting_cost_light[r] * y_light[r, c] +
-        roasting_cost_dark[r] * y_dark[r, c]
-        for r, c in shipping_cost_from_roastery_to_cafe.keys()) + sum(
-            (y_light[j] + y_dark[j]) * shipping_cost_from_roastery_to_cafe[j]
-            for j in shipping_cost_from_roastery_to_cafe.keys()), GRB.MINIMIZE)
+    sum(x[i] * Technician_Cost[i]
+        for i in Technician_Cost.keys()) +
+    sum(maintenance_time[r] * y_light[r, c] +
+        repair_time[r] * y_dark[r, c]
+        for r, c in Travel_Cost.keys()) + sum(
+            (y_light[j] + y_dark[j]) * Travel_Cost[j]
+            for j in Travel_Cost.keys()), GRB.MINIMIZE)
 
 # Conservation of flow constraint
-for r in set(i[1] for i in shipping_cost_from_supplier_to_roastery.keys()):
+for r in set(i[1] for i in Technician_Cost.keys()):
     model.addConstr(
-        sum(x[i] for i in shipping_cost_from_supplier_to_roastery.keys()
+        sum(x[i] for i in Technician_Cost.keys()
             if i[1] == r) == sum(
                 y_light[j] + y_dark[j]
-                for j in shipping_cost_from_roastery_to_cafe.keys()
+                for j in Travel_Cost.keys()
                 if j[0] == r), f"flow_{r}")
 
 # Add supply constraints
-for s in set(i[0] for i in shipping_cost_from_supplier_to_roastery.keys()):
+for s in set(i[0] for i in Technician_Cost.keys()):
     model.addConstr(
-        sum(x[i] for i in shipping_cost_from_supplier_to_roastery.keys()
-            if i[0] == s) <= capacity_in_supplier[s], f"supply_{s}")
+        sum(x[i] for i in Technician_Cost.keys()
+            if i[0] == s) <= capacity_in_technician[s], f"supply_{s}")
 
 # Add demand constraints
-for c in set(i[1] for i in shipping_cost_from_roastery_to_cafe.keys()):
+for c in set(i[1] for i in Travel_Cost.keys()):
     model.addConstr(
-        sum(y_light[j] for j in shipping_cost_from_roastery_to_cafe.keys()
-            if j[1] == c) >= light_coffee_needed_for_cafe[c],
-        f"light_demand_{c}")
+        sum(y_light[j] for j in Travel_Cost.keys()
+            if j[1] == c) >= maintenance_work[c],
+        f"maintenance_demand_{c}")
     model.addConstr(
-        sum(y_dark[j] for j in shipping_cost_from_roastery_to_cafe.keys()
-            if j[1] == c) >= dark_coffee_needed_for_cafe[c],
-        f"dark_demand_{c}")
+        sum(y_dark[j] for j in Travel_Cost.keys()
+            if j[1] == c) >= repair_work[c],
+        f"repair_demand_{c}")
 
 # Optimize model
 model.optimize()
